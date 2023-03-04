85bf8695cd6563527bc62a59f84d03009a30db54
Kala Raut <rautkala3@gmail.com>
2023-03-04 04:20:30 +0000

    elseif

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,8 +20,17 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return x;
-   
+        if(bool == true)
+        {
+            return x;
+        }
+        else if(bool == false)
+        {
+            return y;
+        }  
+        else{
+             return 0;
+        } 
     }
    
-}
+   }
